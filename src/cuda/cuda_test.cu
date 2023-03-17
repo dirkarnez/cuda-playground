3c82d91c2f2a95361934156f3d0cd0bae1c7fd04
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-18 01:55:16 +0800

    - update src/cuda/cuda_test.cu

--- src/cuda/cuda_test.cu
+++ src/cuda/cuda_test.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
@@ -12,6 +13,19 @@ __global__ void vec_add(int *a, int *b)
 
 int main()
 {
+    fprintf(stdout, "Hello world\n");
+    
+    int deviceCount = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+
+    if (error_id != cudaSuccess) {
+        printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id, cudaGetErrorString(error_id));
+        printf("Result = FAIL\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Found %d GPUs\n");
+    }
+    
     const int N = 100;
     // ---- allocate the memory ---- //
     int *a, *b;
@@ -31,4 +45,6 @@ int main()
     {
         cout << a[i] << endl;
     }
+    
+    return 0;
 }
